27be038673fdd2ed04f0a335eb9cef5487b66821
luciens6669 <lstavenhagen@hotmail.com>
2019-05-30 11:33:58 -0600

    add control and settings for menu fonts specifically. Make some names more intuitive in customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -77,7 +77,26 @@ if (class_exists('WP_Customize_Control')) {
 * default system font list found at:
 * https://devhints.io/css-system-font-stack
 */
-$fonts_arr = array(
+$global_fonts_arr = array(
+   __('System Fonts') => array(
+   '-apple-system, BlinkMacSystemFont,"Segoe UI", "Roboto", "Oxygen","Ubuntu", "Cantarell", "Fira Sans","Droid Sans", "Helvetica Neue", sans-serif' => __('Default System Font'),
+   'cursive, sans-serif'   => __('Cursive'),
+   'Courier, Lucida Console, monospace' => __('Courier'),
+   'Lucida Console, Courier, monospace' => __('Lucida Console'),
+   'Monaco, Consolas, "Andale Mono", "DejaVu Sans Mono"' => __('Monaco'),
+   '"Times New Roman", Times, Georgia, serif' => __('Times New Roman'),
+   'Georgia, "Times New Roman", Times, serif' => __('Georgia'),
+   'Verdana, Arial, Helvetica, sans-serif' => __('Verdana'),
+   'Arial, Helvetica, Verdana sans-serif' => __('Arial'),
+   'Helvetica, Verdana, Arial, sans-serif' => __('Helvetica'),
+   'fantasy, sans-serif' => __('Fantasy'),
+   ),
+   __('Google Fonts') => array()
+);
+$menu_fonts_arr = array(
+   __('Inherit From Body') => array(
+      'inherit' => __('Inherit')
+   ),
    __('System Fonts') => array(
    '-apple-system, BlinkMacSystemFont,"Segoe UI", "Roboto", "Oxygen","Ubuntu", "Cantarell", "Fira Sans","Droid Sans", "Helvetica Neue", sans-serif' => __('Default System Font'),
    'cursive, sans-serif'   => __('Cursive'),
@@ -94,7 +113,7 @@ $fonts_arr = array(
    __('Google Fonts') => array()
 );
 
-$style_arr = array(
+$global_style_arr = array(
    __($dont_show) => array(
    'normal' => __('Normal'),
    'italic' => __('Italic'),
@@ -102,13 +121,24 @@ $style_arr = array(
    )
 );
 
+$menu_style_arr = array(
+   __($dont_show) => array(
+   'inherit' => __('Inherit'),
+   'normal' => __('Normal'),
+   'italic' => __('Italic'),
+   )
+);
+
 function register($wp_customize)
 {
-   global $fonts_arr;
-   global $style_arr;
+   global $global_fonts_arr;
+   global $menu_fonts_arr;
+   global $global_style_arr;
+   global $menu_style_arr;
    //1. Define a new section (if desired) to the Theme Customizer
+   
    $wp_customize->add_section(
-      'wileecoder_options',
+      'wileecoder_font_options',
       array(
          'title'       => __('Fonts', 'wileecoder'), //Visible title of section
          'priority'    => 35, //Determines what order this appears in
@@ -116,10 +146,12 @@ function register($wp_customize)
          'description' => __('Allows you to set main font for body.', 'wileecoder'), //Descriptive tooltip
       )
    );
-
+   
    //2. Register new settings to the WP database...
+   // setting for font family generally
+
    $wp_customize->add_setting(
-      'font_ff', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+      'global_font_family', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
       array(
          'default'    => 'sans-serif', //Default setting/value to save
          'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
@@ -129,22 +161,26 @@ function register($wp_customize)
    );
 
    //3. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
-   //$wp_customize->add_control( new WP_Customize_Control( //Instantiate the color control class
+   // control for global, body font family
+
    $wp_customize->add_control(new WP_Customize_Font_Control( //Instantiate the color control class
       $wp_customize, //Pass the $wp_customize object (required)
-      'wileecoder_font_family', //Set a unique ID for the control
+      'wileecoder_global_font_family', //Set a unique ID for the control
       array(
-         'label'      => __('Font Family', 'wileecoder'), //Admin-visible name of the control
-         'settings'   => 'font_ff', //Which setting to load and manipulate (serialized is okay)
+         'label'      => __('Body Font Family', 'wileecoder'), //Admin-visible name of the control
+         'settings'   => 'global_font_family', //Which setting to load and manipulate (serialized is okay)
          'priority'   => 10, //Determines the order this control appears in for the specified section
-         'section'    => 'wileecoder_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+         'section'    => 'wileecoder_font_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
          'type'       => 'fontselect',
-         'choices'    => $fonts_arr
+         'choices'    => $global_fonts_arr
       )
    ));
+
    //4. Register next settings to the WP database...
+   // setting for font style generally
+
    $wp_customize->add_setting(
-      'font_fstyle', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+      'global_font_style', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
       array(
          'default'    => 'normal', //Default setting/value to save
          'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
@@ -154,19 +190,78 @@ function register($wp_customize)
    );
 
    //5. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
-   //$wp_customize->add_control( new Menu_Dropdown_Custom_control( //Instantiate the color control class
+   // control for global font style
+
    $wp_customize->add_control(new WP_Customize_Font_Control( //Instantiate the color control class
       $wp_customize, //Pass the $wp_customize object (required)
-      'wileecoder_font_style', //Set a unique ID for the control
+      'wileecoder_global_font_style', //Set a unique ID for the control
       array(
-         'label'      => __('Font Style', 'wileecoder'), //Admin-visible name of the control
-         'settings'   => 'font_fstyle', //Which setting to load and manipulate (serialized is okay)
+         'label'      => __('Body Font Style', 'wileecoder'), //Admin-visible name of the control
+         'settings'   => 'global_font_style', //Which setting to load and manipulate (serialized is okay)
          'priority'   => 15, //Determines the order this control appears in for the specified section
-         'section'    => 'wileecoder_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+         'section'    => 'wileecoder_font_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
          'type'       => 'fontselect',
-         'choices'    => $style_arr
+         'choices'    => $global_style_arr
       )
    ));
+   //6. Register next settings to the WP database...
+   // setting for menu font family generally
+
+   $wp_customize->add_setting(
+      'menu_font_family', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+      array(
+         'default'    => 'normal', //Default setting/value to save
+         'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
+         'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
+         'transport'  => 'refresh', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
+      )
+   );
+
+   //7. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
+   // control for menu font family
+
+   $wp_customize->add_control(new WP_Customize_Font_Control( //Instantiate the color control class
+      $wp_customize, //Pass the $wp_customize object (required)
+      'wileecoder_menu_font_family', //Set a unique ID for the control
+      array(
+         'label'      => __('Menu Font Family', 'wileecoder'), //Admin-visible name of the control
+         'settings'   => 'menu_font_family', //Which setting to load and manipulate (serialized is okay)
+         'priority'   => 17, //Determines the order this control appears in for the specified section
+         'section'    => 'wileecoder_font_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+         'type'       => 'fontselect',
+         'choices'    => $menu_fonts_arr
+      )
+   ));
+
+   //8. Register next settings to the WP database...
+   // setting for font style generally
+
+   $wp_customize->add_setting(
+      'menu_font_style', //No need to use a SERIALIZED name, as `theme_mod` settings already live under one db record
+      array(
+         'default'    => 'normal', //Default setting/value to save
+         'type'       => 'theme_mod', //Is this an 'option' or a 'theme_mod'?
+         'capability' => 'edit_theme_options', //Optional. Special permissions for accessing this setting.
+         'transport'  => 'refresh', //What triggers a refresh of the setting? 'refresh' or 'postMessage' (instant)?
+      )
+   );
+
+   //9. Finally, we define the control itself (which links a setting to a section and renders the HTML controls)...
+   // control for menu font style
+
+   $wp_customize->add_control(new WP_Customize_Font_Control( //Instantiate the color control class
+      $wp_customize, //Pass the $wp_customize object (required)
+      'wileecoder_menu_font_style', //Set a unique ID for the control
+      array(
+         'label'      => __('Menu Font Style', 'wileecoder'), //Admin-visible name of the control
+         'settings'   => 'menu_font_style', //Which setting to load and manipulate (serialized is okay)
+         'priority'   => 19, //Determines the order this control appears in for the specified section
+         'section'    => 'wileecoder_font_options', //ID of the section this control should render in (can be one of yours, or a WordPress default section)
+         'type'       => 'fontselect',
+         'choices'    => $menu_style_arr
+      )
+   ));
+   
 }
 
 /**
@@ -184,8 +279,12 @@ function header_output()
    <!--Customizer CSS-->
    <style type="text/css">
       <?php 
-      generate_css('body', 'font-family', 'font_ff');
-      generate_css('body', 'font-style', 'font_fstyle');
+      generate_css('body', 'font-family', 'global_font_family');
+      generate_css('body', 'font-style', 'global_font_style');
+      generate_css('.main-navigation', 'font-family','menu_font_family');
+      generate_css('.main-navigation', 'font-style','menu_font_style');
+      generate_css('.footer-navigation', 'font-family','menu_font_family');
+      generate_css('.footer-navigation', 'font-style','menu_font_style');
       ?>
    </style>
    <!--/Customizer CSS-->
